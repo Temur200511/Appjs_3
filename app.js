7d481720f945a146315971d49a13c8d07dc7a2af
Temur200511 <tziyodullayev61@gmail.com>
2024-12-03 18:07:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,102 @@
+// 1) let a = prompt("birinchi sonni kiriting");
+// let b = prompt("ikkinchi sonni kiriting");
+// let c = prompt("uchinchi sonni kiriting");
+
+// if (a > b && a > c) {
+//     console.log(a);
+// } else if (b > a && b > c) {
+//     console.log(b);
+// } else {
+//     console.log(c);
+// }   
+
+
+
+// 2) let a = prompt("birinchi sonni kiriting");
+// let b = prompt("ikkinchi sonni kiriting");
+// let c = prompt("uchinchi sonni kiriting");
+
+// if (a+b > c+a) {
+//     console.log(a, b);            
+// } else if (b+c > a+c) {
+//     console.log(b, c);
+// } else {
+//     console.log(c, a);
+// }
+
+
+
+
+
+// 3) let son = prompt("Sonni kiriting");
+
+
+// if(son > 0 && son % 2 == 1) {
+//     console.log("Musbat toq son");        
+// } else if (son > 0 && son % 2 ==0) {
+//     console.log("musbat juft son");
+    
+// }
+// else if (son < 0 && son % 2 == 1) {
+//     console.log("manfiy toq son");        
+// } else if (son < 0 && son % 2 ==0) {
+//     console.log("manfiy juft son");
+// }
+
+// else if(son == 0) {
+//     console.log("son nolga teng");
+// }
+
+
+
+
+
+
+// let Oyraqami = prompt("Oy raqamini kiriting");
+
+// switch (Oyraqami) {
+//     case "1":
+//         console.log("31 kun");
+//         break;
+//     case "2":
+//         console.log("28 kun");
+//         break;
+//     case "3":
+//         console.log("31 kun");
+//         break;
+//     case "4":
+//         console.log("30 kun");
+//         break;
+//     case "5":
+//         console.log("31 kun");
+//         break;
+//     case "6":
+//         console.log("30 kun");
+//         break;
+//     case "7":
+//         console.log("31 kun");
+//         break;
+//     case "8":
+//         console.log("31 kun");
+//         break;
+//     case "9":
+//         console.log("30 kun");
+//         break;
+//     case "10":
+//         console.log("31 kun");
+//         break;
+//     case "11":
+//         console.log("30 kun");
+//         break;
+//     case "12":
+//         console.log("31 kun");
+//         break;
+    
+//         default:
+//         console.log("Bunday oy raqami mavjud emas");
+//         break;
+// }
+
+
+
+
