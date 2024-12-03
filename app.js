c7a1f014be2a46591e2e2d4510f19de78c258c50
Temur200511 <tziyodullayev61@gmail.com>
2024-12-03 19:52:31 +0500

    "full"

--- app.js
+++ app.js
@@ -100,3 +100,54 @@
 
 
 
+
+// let a = Number(prompt("Sonni kiriting"));
+
+// if (a>0 && a<999) {
+//     let yuz=Math.floor(a/100);
+//     let on=Math.floor((a%100)/10);
+//     let bir=a%10;
+
+//     let yuzlik="",onlik="",birlik="";
+
+//     switch (yuz) {
+//         case 1: yuzlik="yuz"; break;    
+//         case 2: yuzlik="ikki yuz"; break;
+//         case 3: yuzlik="uch yuz"; break;
+//         case 4: yuzlik="to'rt yuz"; break;
+//         case 5: yuzlik="besh yuz"; break;
+//         case 6: yuzlik="olti yuz"; break;
+//         case 7: yuzlik="yetti yuz"; break;
+//         case 8: yuzlik="sakkiz yuz"; break;
+//         case 9: yuzlik="to'qqiz yuz"; break;
+//     }
+
+//     switch (on) {
+//         case 1: onlik="o'n"; break;
+//         case 2: onlik="yigirma"; break;
+//         case 3: onlik="o'ttiz"; break;
+//         case 4: onlik="qirq"; break;
+//         case 5: onlik="ellik"; break;
+//         case 6: onlik="oltmish"; break;
+//         case 7: onlik="yetmish"; break;
+//         case 8: onlik="sakson"; break;
+//         case 9: onlik="to'qson"; break;
+//     }
+
+//     switch (bir) {
+//         case 1: birlik="bir"; break;
+//         case 2: birlik="ikki"; break;    
+//         case 3: birlik="uch"; break;
+//         case 4: birlik="to'rt"; break;
+//         case 5: birlik="besh"; break;
+//         case 6: birlik="olti"; break;
+//         case 7: birlik="yetti"; break;
+//         case 8: birlik="sakkiz"; break;
+//         case 9: birlik="to'qqiz"; break;
+//     }
+
+//     alert(`Kiritilgan son: ${a} \nNatija:${yuzlik} ${onlik} ${birlik}`.trim());
+
+// }
+
+// alert("Son 1-999 orasida bo'lishi kerak");
